aeec42b18039e7fa306b7237e072d023e7ed0183
Vitor <vitorpaiva130@gmail.com>
2026-07-05 19:04:27 -0300

    teste sem erro deploy

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  outputFileTracingRoot: require('path').join(__dirname, '../../'),
   typescript: {
     ignoreBuildErrors: false,
   },
