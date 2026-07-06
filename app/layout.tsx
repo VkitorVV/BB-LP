7ef3bdddac81211cd928481e633dd384c29a68e7
Vitor <vitorpaiva130@gmail.com>
2026-07-05 23:43:54 -0300

    CLARITY

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <SpeedInsights />
         <Analytics />
       </body>
-      {/* Google Analytics GA4 */}
+      {/* Microsoft Clarity */}
+      <Script id="clarity-init" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "xhzxqz3hq9");
+        `}
+      </Script>
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-GM3SG4JEKT"
         strategy="afterInteractive"
